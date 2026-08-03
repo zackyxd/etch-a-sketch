e277ab2937a7b1e7d0d6034ae50fcb00eba7ff25
zackyxd <zacharywalton501@gmail.com>
2026-08-02 20:20:50 -0700

    Fix code to show correct 16x16 grid initially and take in user input to make x squares per side.

--- script.js
+++ script.js
@@ -1,16 +1,34 @@
-const container = document.createElement("div");
-container.classList.add("container");
+const btnPrompt = document.createElement("button");
+btnPrompt.innerText = "Click to update squares per side.";
+document.body.appendChild(btnPrompt);
+btnPrompt.addEventListener("click", (event) => {
+  const amt = prompt("How many squares should show per side?");
+  createGrid(amt);
+});
 
-const colors = ["red", "blue", "green", "yellow", "purple", "gray", "black", "orange"];
+function createGrid(squares = 16) {
+  const deleteGrid = document.querySelector(".container");
+  if (deleteGrid) deleteGrid.remove();
+  const container = document.createElement("div");
+  container.classList.add("container");
 
-for (let i = 0; i < 16; i++) {
-  const grid = document.createElement("div");
-  grid.classList.add("square");
-  grid.addEventListener("mouseover", (event) => {
-    const randInt = Math.floor(Math.random() * colors.length + 1);
-    grid.style.backgroundColor = colors[randInt];
-  });
-  container.appendChild(grid);
+  document.body.appendChild(container);
+  const colors = ["red", "blue", "green", "yellow", "purple", "gray", "black", "orange"];
+  for (let i = 0; i < squares; i++) {
+    for (let j = 0; j < squares; j++) {
+      const grid = document.createElement("div");
+      grid.classList.add("square");
+      grid.style.width = `${100 / squares}%`;
+      grid.style.height = `${100 / squares}%`;
+      container.appendChild(grid);
+      const gridSelector = document.querySelectorAll(".square");
+      console.log(gridSelector);
+      grid.addEventListener("mouseover", (event) => {
+        const randInt = Math.floor(Math.random() * colors.length + 1);
+        grid.style.backgroundColor = colors[randInt];
+      });
+    }
+  }
 }
 
-document.body.appendChild(container);
+createGrid();
